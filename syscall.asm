edefa2e1f231e6b4e6e9c2d51f107ff197f6fd14
Diego Rojas <Diego-27ro@outlook.com>
2021-11-20 02:29:12 -0500

    funcional pedir dinero

--- syscall.asm
+++ syscall.asm
@@ -24,24 +24,42 @@ input_continue_ingreso: .asciiz "Desea Ingresar mas Dinero?\n 1.Si\nInserte cual
 .text
 
 #main
-#maquina:
-#	li $v0, 4
-#	la $a0, input_tipo_dinero
-#	syscall	
-#	li $v0, 5	#pide que se ingrese un numero entero
-#	syscall
-#	move $t0,$v0	#$t0 almacena la opcion de si va ingresar billetes o monedas
-#	 
-#	beq $t0,1,cuentaBilletes
+maquina:
+	li $v0, 4
+	la $a0, input_tipo_dinero
+	syscall	
+	li $v0, 5	#pide que se ingrese un numero entero
+	syscall
+	move $t0,$v0	#$t0 almacena la opcion de si va ingresar billetes o monedas
+	 
+	beq $t0,1,cuentaBilletes
+	li $t1,1 #vuelvo a MAQUINA
 	#beq $t0,2,cuentaMonedas
 	#beq $t0,3,salir
 	
 cuentaBilletes:
+	
+	addi $sp,$sp,4
+	sw $ra,0($sp)
 	li $a0, 0	#acumulador
 	jal loopPedirBilletes
 	move $t0,$v0  #t0 almacena el total de dinero ingresado
 	li $t1,1 #vuelvo al main
 	
+	
+	lw $ra,0($sp)
+	addi $sp,$sp,-4
+	li $v0,1 #retorno de cuenta billetes
+	
+	
+	
+	#codigo que hacer una vez tengo el total de dinero ingresado en $t1
+	
+	
+	
+	
+	#a esta funcion no hay que hacerle jr ra
+	jal maquina #para volver al menu principal
 	
 loopPedirBilletes:
 	
@@ -70,7 +88,7 @@ loopPedirBilletes:
 	lw $a0,0($sp)
 	lw $ra,4($sp)
 	
-	move $a0,$s0
+	move $a0,$s0	#almaceno el valor del billete ingresado para mandarlo como argumento al loop de billetes
 	lw $s0,8($sp)
 	addi $sp,$sp,-12
 	
@@ -78,8 +96,6 @@ loopPedirBilletes:
 	
 	move $v0,$a0
 	jr $ra 
-	
-	
 	
 pedirDineroBilletes:
 	li $v0, 4
@@ -91,34 +107,23 @@ pedirDineroBilletes:
 	
 	jr $ra	#retorno el valor del billete ingresado
 
-#main:
-
-	
-			
-
 #etiqueta de validación del arreglo billetes  beq valor, 1,ValidarBilletes 
 #s1 tiene el valor ingresado del billete o moneda
 validarBilletes:
 	la $t0, billetes #arreglo de billetes A=[1,5,10,20]
-	li $t2,1 #temp = 0
 	li $t3,4 #constante length = 4
 	slt $t4, $a1, $t3 # i<4 , 1 True, 0 False
 	beq $t4,$zero,pedirDineroBilletes #if(1/0==0) 
 	sll $t5, $a1,2
 	add $t5,$t0, $t5 # La base del arreglo es billetes
 	lw $t6, 0($t5) #Billetes[i]
-	addi $t1,$a1,1 # i+1
-	bne $t6,$a0,validarBilletes 
-	beq $t6,$a0,DineroValido #t6 es el valor el arreglo en i posicion, $a0 es el valor igresado por consola 
-	move $v0,$t2
+	addi $a1,$a1,1 # i+1
+	bne $t6,$a0,validarBilletes 	
+	move $v0,$a0
 	jr $ra
 	
 	
-DineroValido:
-	li $t2,1 #temp = 1
-	move $v0,$t2
-	jr $ra
-	 
+
 	
 
 
